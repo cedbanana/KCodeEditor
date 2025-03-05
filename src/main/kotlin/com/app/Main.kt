b8335a69d73bc7c45d2091a29c2764c92553efde
cedbanana <86929237+cedbanana@users.noreply.github.com>
2025-03-05 17:03:36 +0100

    Initial start with a stage split in 2 parts, coding area and output area

--- src/main/kotlin/com/app/Main.kt
+++ src/main/kotlin/com/app/Main.kt
@@ -1,11 +1,30 @@
 package com.app
 
 import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.ListView
+import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
+import org.fxmisc.richtext.CodeArea
 
-class ScriptRunnerApp: Application(){
+class CodeEditor: Application(){
     override fun start(primaryStage: Stage) {
-        //TODO
-    }
+        val codeArea = CodeArea() // will write code in
 
+        val outputList=ListView<String>() // will show the output
+
+        val borderPane = BorderPane()
+
+        borderPane.center = codeArea
+        borderPane.right = outputList
+        val scene = Scene(borderPane,800.0, 600.0)
+        primaryStage.scene = scene
+        primaryStage.title = "Code Editor"
+        primaryStage.show()
+
+    }
+}
+
+fun  main() {
+    Application.launch(CodeEditor::class.java)
 }
