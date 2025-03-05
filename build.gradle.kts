15e0b70b9cb3ad4c11fd02a225f36720c37e4559
cedbanana <86929237+cedbanana@users.noreply.github.com>
2025-03-05 16:24:41 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("org.openjfx.javafxplugin") version "0.0.13"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.fxmisc.richtext:richtextfx:0.11.0")
+    implementation("org.openjfx:javafx-controls:17.0.1")
+}
+
+javafx {
+    version = "17"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(19)
+}
+application {
+    mainClass.set("com.app.MainKt")
+}
---
+++ src/main/kotlin/com/app/Main.kt
@@ -0,0 +1,11 @@
+package com.app
+
+import javafx.application.Application
+import javafx.stage.Stage
+
+class ScriptRunnerApp: Application(){
+    override fun start(primaryStage: Stage) {
+        //TODO
+    }
+
+}
